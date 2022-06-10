f9fd83a60ef3ff00c9bab71fc7540d83bffb912b
pascal <kongwutianwen@163.com>
2022-06-10 14:51:41 +0000

    fix: browser github pages error

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 import nestPlugin from 'postcss-nested';
+import { processExpression } from '.pnpm/@vue+compiler-core@3.2.37/node_modules/@vue/compiler-core';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? 'ps' : '',
   plugins: [vue({
     script: {
       refTransform: true,
